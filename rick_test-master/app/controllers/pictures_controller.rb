16113efb3beeff3b8a918ac398e4be4845a787bf
Nicholas Michel <nickmichel100@gmail.com>
2016-09-08 15:43:48 -0400

    working image upload and storage, very basic image merging

--- rick_test-master/app/controllers/pictures_controller.rb
+++ rick_test-master/app/controllers/pictures_controller.rb
@@ -7,7 +7,6 @@ class PicturesController < ApplicationController
   def new
     @picture = Picture.new(params[:picture])
     if params[:image_id].present?
-      byebug
       preloaded = Cloudinary::PreloadedFile.new(params[:image_id])
       raise "Invalid upload signature" if !preloaded.valid?
       @picture.url = preloaded.identifier
@@ -16,5 +15,11 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(url: @url)
+    redirect to: "pictures#index"
+  end
+
+  def upload
+    Picture.create(url: "#{params[:address]}.#{params[:format]}")
+    redirect_to "/"
   end
 end
--- rick_test-master/config/routes.rb
+++ rick_test-master/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pictures
+  post '/pictures/upload', to: 'pictures#upload'
   root to: "pictures#index"
 end
