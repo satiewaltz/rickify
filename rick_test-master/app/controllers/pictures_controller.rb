f2d5f046660caafa7df1e4cbcf1f55fdd8421fa5
Nicholas Michel <nickmichel100@gmail.com>
2016-09-08 11:21:23 -0400

    keys

--- rick_test-master/app/controllers/pictures_controller.rb
+++ rick_test-master/app/controllers/pictures_controller.rb
@@ -6,15 +6,15 @@ class PicturesController < ApplicationController
 
   def new
     @picture = Picture.new(params[:picture])
-  end
-
-  def create
-    render plain: params[:image_id]
     if params[:image_id].present?
       byebug
       preloaded = Cloudinary::PreloadedFile.new(params[:image_id])
       raise "Invalid upload signature" if !preloaded.valid?
       @picture.url = preloaded.identifier
     end
+  end
+
+  def create
+    @picture = Picture.new(url: @url)
   end
 end
