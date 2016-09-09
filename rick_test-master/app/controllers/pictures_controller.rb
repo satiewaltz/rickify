aa6868a9b7becaa4817f8788b27550bb69b03419
Dave Barthly <davebarthly@gmail.com>
2016-09-08 22:59:26 -0400

    Ajax works wooooo

--- rick_test-master/app/controllers/pictures_controller.rb
+++ rick_test-master/app/controllers/pictures_controller.rb
@@ -2,6 +2,16 @@ class PicturesController < ApplicationController
 
   def index
     @pictures = Picture.all
+    require 'httparty'
+    derp = "http://i.imgur.com/Ysq0xY1.jpg"
+    response = HTTParty.get "https://apicloud-facerect.p.mashape.com/process-url.json?features=true&url=" + derp,
+      headers:{
+        "X-Mashape-Key" => "",
+        "Accept" => "text/plain"
+      }
+    require 'betaface'
+    p response
+    puts response
   end
 
   def new
@@ -11,7 +21,6 @@ class PicturesController < ApplicationController
   def create
     render plain: params[:image_id]
     if params[:image_id].present?
-      byebug
       preloaded = Cloudinary::PreloadedFile.new(params[:image_id])
       raise "Invalid upload signature" if !preloaded.valid?
       @picture.url = preloaded.identifier
