33708387f1c188a89060b888046c379058fe1dc0
Nicholas Michel <nickmichel100@gmail.com>
2016-09-09 17:34:24 -0400

    rickify working

---
+++ samples/basic/config.rb
@@ -0,0 +1,8 @@
+# Configuration values for your account are available
+# in Cloudinary's Management Console (https://cloudinary.com/console)
+
+Cloudinary.config do |config|
+  config.cloud_name = ""
+  config.api_key = ""
+  config.api_secret = ""
+end
