8e634fb59679758c4a0f2cc65fb2ed9221920684
Dave Barthly <davebarthly@gmail.com>
2016-09-11 15:59:26 -0400

    Seperate folders from master

--- rickify_redone/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :pictures
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/pictures/upload', to: 'pictures#upload'
-  root to: "pictures#index"
-  get '/frs', to: 'pictures#frs'
-end
